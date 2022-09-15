a978ce37b13e8d32173d68a49cc8481d2e9836bb
Mariah <mariahschock@gmail.com>
2022-09-15 15:07:15 -0700

    GET:ID test is passing

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
+const crypto = require('crypto');
+const SimpleDb = require('../lib/simple-db');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
@@ -12,8 +14,16 @@ describe('simple database', () => {
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
-
+  it('GET:ID returns object by ID', async () => {
+    const cat = {
+      name: 'Leo',
+      age: 5
+    };
+    const id = crypto.randomBytes(8).toString('hex');
+    await fs.writeFile(`${TEST_DIR}/${id}.json`, JSON.stringify(cat));
+    const db = new SimpleDb(TEST_DIR);
+    const result = await db.get(id);
+    expect(result).toEqual(cat);
   });
 
 });
